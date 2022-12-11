22c106027bb727489ebb41148f0bc6e56fc6d442
Alina Kositsyna <a.y.kositsyna@gmail.com>
2022-12-12 00:21:31 +0700

    Добавьте файлы проекта.

---
+++ assignment8/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment8
+{
+    internal class Customer
+    {
+        public string FullName;
+        public byte GentleRate;
+        public GentleSmartphone Smartphone;
+        public Transformator TransformModule;
+
+        public Customer()
+        { }
+    }
+}
---
+++ assignment8/Factory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static assignment8.GentleSmartphone;
+using static assignment8.Customer;
+
+namespace assignment8
+{
+    internal class Factory
+    {
+        public List<GentleSmartphone> Smartphones { get; set; }
+        public List<Customer> Customers { get; set; }
+
+        public Factory()
+        {
+
+        }
+
+        public static void SaleSmartphone()
+        {
+
+        }
+    }
+}
